a7138be2b19247314741a9afd9341016aea9b619
Adeyemi Samuel <adeyemifrodo@gmail.com>
2023-11-19 14:35:16 -0800

    Done

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,18 +1,14 @@
 -- MySQL setup script
 -- Database and user information
 
-SET hbnb_test_db = 'hbnb_test_db';
-SET hbnb_test = 'hbnb_test';
-SET hbnb_test_pwd = 'hbnb_test_pwd';
-
 -- Create the database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 
 -- Create the user if it doesn't exist
-CREATE USER IF NOT EXISTS hbnb_test@'localhost' IDENTIFIED BY hbnb_test_pwd;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
 -- Grant privileges to the user on the database
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO hbnb_test@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 
 -- Grant SELECT privilege on the performance_schema database
-GRANT SELECT ON performance_schema.* TO hbnb_test@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
