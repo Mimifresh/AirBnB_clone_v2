dc197a6c64e2e80811cf96d75b11b1043399c995
Mimifresh <mimigolld@gmail.com>
2023-11-22 07:09:02 +0100

    fixed bugs

--- models/place.py
+++ models/place.py
@@ -6,14 +6,14 @@ from sqlalchemy import Column, String, ForeignKey, Integer, Float
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = 'place'
-    city_id = Column(string(60), ForeignKey('cities.id'), nullable=False)
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
     description = Column(String(1024), nullable=True)
-    number_rooms = Column(Integer, defualt=0, nullable=False)
-    number_bathrooms = Column(Integer, defualt=0, nullable=False)
-    max_guest = Column(Integer, defualt=0, nullable=False)
-    price_by_night = Column(Integer, defualt=0, nullable=False)
+    number_rooms = Column(Integer, default=0, nullable=False)
+    number_bathrooms = Column(Integer, default=0, nullable=False)
+    max_guest = Column(Integer, default=0, nullable=False)
+    price_by_night = Column(Integer, default=0, nullable=False)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
     amenity_ids = []
