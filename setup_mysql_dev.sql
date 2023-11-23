405ed8d01da4f268833e5151433b9fcbb4389b1c
Mimifresh <mimigolld@gmail.com>
2023-11-22 18:05:42 -0800

    fixed relationship issues

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,19 @@
--- script that prepares a MySQL server
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
 -- creates user
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON hbnb_dev_db . * TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON performance_schema . * TO 'hbnb_dev'@'localhost';
 
+-- Grant privileges on the hbnb_dev_db database
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+-- Refresh the privilege tables
+FLUSH PRIVILEGES;
+
+-- Grant SELECT privilege on the mysql schema
+GRANT SELECT ON mysql.* TO 'hbnb_dev'@'localhost';
+
+-- Grant SELECT privilege on the performance_schema schema
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+-- Refresh the privilege tables again
+FLUSH PRIVILEGES;
